6a1f26dc46b30dd3c5e650c307facddc6c1e320c
Jitae9605 <dlwlxo3819@naver.com>
2022-03-31 11:58:21 +0900

    - 품목 헤드, cpp 나눔 - handler .h .cpp 생성 - 메인문 나눔

---
+++ MiniProject/source/handler.h
@@ -0,0 +1,35 @@
+#ifndef __HANDLER_H__
+#define __HANDLER_H__
+
+#include"Item.h"
+
+
+class Handler
+{
+private:
+public:
+	Handler(void);
+	// 샘플데이터입력
+	void Init_Sample_ItemInfo(void);
+
+	// 메인메뉴
+	void Menu_Main(void);
+
+	// BOM
+	void Menu_BOM();
+	//void Add_BOM();
+	//void Delete_BOM();
+	//void Show_BOM();
+	
+	//// 생산계획
+	void Menu_ManufacturePlanning();
+	//void Add_ManufacturePlanning();
+	//void Delete_ManufacturePlanning();
+	//void Show_ManufacturePlanning();
+	
+	//// 불량정보
+	//void Show_NG_List();
+
+};
+
+#endif 
